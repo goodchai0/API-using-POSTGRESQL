73c12a316b9dbe392e142b1c391b1f011347593b
Amit <www.amitkewot@gmail.com>
2022-12-26 17:40:40 +0530

    first commit

---
+++ src/student/controller.js
@@ -0,0 +1,74 @@
+const { Pool } = require('pg');
+const pool=require('../../db.js');
+const queries=require('./queries');
+
+const getStudents=(req,res)=>{
+   pool.query(queries.getStudents,(error,results)=>{
+    if(error)throw error;
+    res.status(200).json(results.rows);
+   })
+}
+
+const getStudentsById=(req,res)=>{
+    const id=parseInt(req.params.id);
+    pool.query(queries.getStudentsById,[id],(error,results)=>{
+     if(error)throw error;
+     res.status(200).json(results.rows);
+    })
+ }
+ const addStudent=(req,res)=>{
+    const {name,email,age,dob}=req.body;
+
+    //check if email exists
+    pool.query(queries.checkEmailExists,[email],(error,results)=>{
+        if(results.rows.length){
+            res.send("Email already exists. ");
+        }
+
+        //add student to db
+        pool.query(queries.addStudent,[name,email,age,dob],(error,results)=>{
+            if(error) throw error;
+            res.status(201).send("student Created successfully");
+        })
+    })
+ }
+
+ const removeStudent=(req,res)=>{
+    const id=parseInt(req.params.id);
+    
+    pool.query(queries.getStudentsById,[id],(error,results)=>{
+        const noStudentFound=!results.rows.length;
+        if(noStudentFound){
+            res.send('Student does not exist in the database');
+        }
+        pool.query(queries.removeStudent,[id],(error,results)=>{
+            if(error)throw error;
+            res.status(200).send("Student removed successfully");
+        })
+    })
+ }
+ 
+const updateStudent=(req,res)=>{
+    const id=parseInt(req.params.id);
+    const {name}=req.body;
+
+    pool.query(queries.getStudentsById,[id],(error,results)=>{
+        const noStudentFound=!results.rows.length;
+        if(noStudentFound){
+            res.send("STUDENT DOES NOT EXISTS IN THE DATABASE");
+        }
+
+        pool.query(queries.updateStudent,[name,id],(error,results)=>{
+            if(error) throw error;
+            res.status(200).send("Student got updated successfully")
+        })
+    })
+}
+
+module.exports={
+    getStudents,
+    getStudentsById,
+    addStudent,
+    removeStudent,
+    updateStudent,
+}
